6edca0abbeb85c11e6bcb1c69ea5b5556a14110c
Евгения <zhoonya1@rambler.ru>
2020-08-13 14:23:09 +0300

    84(hook)

--- my-app/src/Components/Content/Profile/ProfileStatus.jsx
+++ my-app/src/Components/Content/Profile/ProfileStatus.jsx
@@ -1,54 +1,90 @@
-import React from "react";
+import React, {useState} from "react";
 
-export default class ProfileStatus extends React.Component {
+export default function ProfileStatus (props) {
 
-    state = {
-        editMode: false,
-        status: this.props.status,
+    let [editMode, setEditMode] = useState(false);
+    let [status, setStatus] = useState(props.status);
+
+    const activateEditMode = () => {
+        setEditMode(true);
     };
 
-    activateEditMode = () => {
-        this.setState({
-            editMode: true,
-        });
-
+    const deactivateEditMode = () => {
+        setEditMode(false);
+        props.updateStatus(status);
     };
 
-    deactivateEditMode = () => {
-        this.setState({
-            editMode: false,
-        });
-        this.props.updateStatus(this.state.status);
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value);
     };
 
-    onStatusChange = (e) => {
-        this.setState({
-            status: e.currentTarget.value,
-        })
-    };
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if (prevProps.status !== this.props.status) {
-            this.setState({
-                status: this.props.status
-            });
-        }
+    if (!editMode) {
+        return (
+            <p onClick={activateEditMode}>{props.status || "-------"}</p>
+        )
+    } else {
+        return (
+            <div>
+                <input autoFocus={true} onBlur={(e) => {
+                    deactivateEditMode(e)
+                }} type="text" value={status}
+                       onChange={onStatusChange}/>
+            </div>
+        )
     }
 
-    render() {
-        if (!this.state.editMode) {
-            return (
-                <p onClick={this.activateEditMode}>{this.props.status || "-------"}</p>
-            )
-        } else {
-            return (
-                <div>
-                    <input autoFocus={true} onBlur={(e) => {
-                        this.deactivateEditMode(e)
-                    }} type="text" value={this.state.status}
-                    onChange={this.onStatusChange}/>
-                </div>
-            )
-        }
-    };
+
 }
+
+// export default class ProfileStatus extends React.Component {
+//
+//     state = {
+//         editMode: false,
+//         status: this.props.status,
+//     };
+//
+//     activateEditMode = () => {
+//         this.setState({
+//             editMode: true,
+//         });
+//
+//     };
+//
+//     deactivateEditMode = () => {
+//         this.setState({
+//             editMode: false,
+//         });
+//         this.props.updateStatus(this.state.status);
+//     };
+//
+//     onStatusChange = (e) => {
+//         this.setState({
+//             status: e.currentTarget.value,
+//         })
+//     };
+//
+//     componentDidUpdate(prevProps, prevState, snapshot) {
+//         if (prevProps.status !== this.props.status) {
+//             this.setState({
+//                 status: this.props.status
+//             });
+//         }
+//     }
+//
+//     render() {
+//         if (!this.state.editMode) {
+//             return (
+//                 <p onClick={this.activateEditMode}>{this.props.status || "-------"}</p>
+//             )
+//         } else {
+//             return (
+//                 <div>
+//                     <input autoFocus={true} onBlur={(e) => {
+//                         this.deactivateEditMode(e)
+//                     }} type="text" value={this.state.status}
+//                     onChange={this.onStatusChange}/>
+//                 </div>
+//             )
+//         }
+//     };
+// }
